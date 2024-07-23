ef3cf0c907f0a8a7a8a9311f23ecf4ee7e8bfa76
edineusa <edineusa08@gmail.com>
2024-07-23 11:21:05 -0300

    first commit

---
+++ src/main/java/SpringBootFramework/DesafioDeProjetoApplication.java
@@ -0,0 +1,13 @@
+package SpringBootFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDeProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDeProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringBootFramework/DesafioDeProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDeProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
